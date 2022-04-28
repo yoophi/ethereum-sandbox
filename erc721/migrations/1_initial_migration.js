874ac2ef037586d87784eb6bad6b9439032549ec
Pyunghyuk Yoo <yoophi@gmail.com>
2022-04-28 21:39:03 +0900

    ✨ ADD: base codes for NFT

--- erc721/migrations/1_initial_migration.js
+++ erc721/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const HelloNFT = artifacts.require("HelloNFT");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(HelloNFT);
 };
